425d9a5941250130adbca1e107950137ff59a5b3
Eric Frey <ericlfrey@gmail.com>
2022-10-04 21:29:37 -0500

    Finished styling. Added IDs. Began writing JS for button functions.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log('Calculator!');
+
+const display = document.getElementById('display');
+const one = document.getElementById('one');
+
+
+function clickOne() {
+  display.innerHTML = 1;
+}
