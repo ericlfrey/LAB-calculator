1c50240de66ad2a6b86fb80a748cbf1c2dc3431c
Eric Frey <ericlfrey@gmail.com>
2022-10-13 16:07:59 -0500

    Still coding via the tut

--- main.js
+++ main.js
@@ -8,9 +8,10 @@ keys.addEventListener('click', e => {
     const action = key.dataset.action;
     const keyContent = key.textContent;
     const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType
     //check to see if button is a number key
     if (!action) {
-      if (displayedNum === '0') {
+      if (displayedNum === '0' || previousKeyType === 'operator') {
         display.textContent = keyContent;
       } else {
         display.textContent = displayedNum + keyContent;
@@ -24,6 +25,9 @@ keys.addEventListener('click', e => {
       action === 'divide'
     ) {
       key.classList.add('is-depressed');
+      calculator.dataset.previousKeyType = 'operator';
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
     }
     //check to see about other specific keys
     if (action === 'decimal') {
@@ -36,7 +40,26 @@ keys.addEventListener('click', e => {
       console.log('delete key!');
     }
     if (action === 'calculate') {
-      console.log('equal key!');
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
+
+      const calculate = (n1, operator, n2) => {
+        let result = '';
+
+        if (operator === 'add') {
+          result = n1 + n2;
+        } else if (operator === 'subtract') {
+          result = n1 - n2;
+        } else if (operator === 'multiply') {
+          result = n1 * n2;
+        } else if (operator === 'divide') {
+          result = n1 / n2;
+        }
+        return result;
+      }
     }
     // Remove .is-depressed class from all keys
     Array.from(key.parentNode.children)
